9756a21b45ed0450890b62eb68300e574516fffc
Blassony <58885829+Blassony@users.noreply.github.com>
2020-04-30 12:49:32 +0000

    first commit

---
+++ js/validation.js
@@ -0,0 +1,55 @@
+ $(document).ready(function(){
+        $('#send_message').click(function(e){
+            
+            e.preventDefault();
+            
+            var error = false;
+            var name = $('#name').val();
+            var email = $('#email').val();
+			var phone = $('#phone').val();
+            var message = $('#message').val();
+			
+			$('#name,#email,#phone,#message').click(function(){
+				$(this).removeClass("error_input");
+			});
+            
+            if(name.length == 0){
+                var error = true;
+                $('#name').addClass("error_input");
+            }else{
+                $('#name').removeClass("error_input");
+            }
+            if(email.length == 0 || email.indexOf('@') == '-1'){
+                var error = true;
+                $('#email').addClass("error_input");
+            }else{
+                $('#email').removeClass("error_input");
+            }
+			if(phone.length == 0){
+                var error = true;
+                $('#phone').addClass("error_input");
+            }else{
+                $('#phone').removeClass("error_input");
+            }
+            if(message.length == 0){
+                var error = true;
+                $('#message').addClass("error_input");
+            }else{
+                $('#message').removeClass("error_input");
+            }
+            
+            if(error == false){
+                $('#send_message').attr({'disabled' : 'true', 'value' : 'Sending...' });
+                
+                $.post("email.php", $("#contact_form").serialize(),function(result){
+                    if(result == 'sent'){
+                         $('#submit').remove();
+                        $('#mail_success').fadeIn(500);
+                    }else{
+                        $('#mail_fail').fadeIn(500);
+                        $('#send_message').removeAttr('disabled').attr('value', 'Send The Message');
+                    }
+                });
+            }
+        });    
+    });
